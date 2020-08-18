bb6f6808e5e754acea6b7951f5674e0c8bdf5941
smtkesici <170202013@kocaeli.edu.tr>
2020-08-18 15:00:02 +0300

    db

---
+++ db.sql
@@ -0,0 +1,29 @@
+--PSQL SHELL
+
+--for help \?
+
+--list database \l
+
+--create database CREATE DATABASE database_name
+
+--connect database \c database_name
+
+--list all tables \d
+
+
+CREATE TABLE products (
+    id INT,
+    name VARCHAR(50),
+    price INT,
+    on_sale boolean
+);
+
+
+CREATE TABLE restaurants (
+    id BIGSERIAL NOT NULL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    location VARCHAR(50) NOT NULL,
+    price_range INT NOT NULL check(price_range >= 1 and price_range <=5)
+);
+
+INSERT INTO restaurants (id,name,location,price_range) values (123,'mcdonals','new york',3)
